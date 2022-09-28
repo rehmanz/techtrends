b24201c670e9071a41e7eed0c338722d1834d111
rehmanz <rehmanz@yahoo.com>
2022-09-28 09:25:13 -0700

    Fixed unit test for metrics endpoint.

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,4 @@ def test_get_root_endpoint():
     conn = http.client.HTTPConnection("localhost", 3111)
     conn.request("GET", "/metrics")
     res = conn.getresponse()
-    assert "UserCountActive" in res.read().decode("utf-8")
+    assert "success" in res.read().decode("utf-8")
